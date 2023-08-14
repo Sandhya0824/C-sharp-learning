271e04fa01e8d4965ef45535e187d29ef5787238
Sandhya0824 <sandhyaa246789@gmail.com>
2023-08-14 17:25:45 +0530

    updated till struct

--- PracticeCode/StructExample.cs
+++ PracticeCode/StructExample.cs
@@ -7,23 +7,39 @@ using System.Threading.Tasks;
 
 namespace PracticeCode
 {
-    internal class StructExample
+    public struct Person
     {
-        public int width, height;
+        public string Name;
+        public int Age;
+        public int Weight;
+    }
 
-        public StructExample(int w, int h)
+    public class StructExample
+    { 
+        static void Main(string[] args)
         {
-            width = w;
-            height = h;
-        }
-        public void areaOfRectangle()
-        {
-            Console.WriteLine("Area of Rectangle is: " + (width * height));
-        }
-        public static void Main()
-        {
-            StructExample r = new StructExample(5, 6);
-            r.areaOfRectangle();
+
+            Person P1; //no need of new keyword
+
+            P1.Name = "Harsh";
+            P1.Age = 21;
+            P1.Weight = 80;
+
+            Person P2;
+
+            P2 = P1; //new structure is created with "=" operator
+
+            Console.WriteLine("Values Stored in P1");
+            Console.WriteLine("Name: " + P1.Name);
+            Console.WriteLine("Age: " + P1.Age);
+            Console.WriteLine("Weight: " + P1.Weight);
+            Console.WriteLine();
+
+            Console.WriteLine("Values Stored in P2");
+            Console.WriteLine("Name: " + P2.Name);
+            Console.WriteLine("Age: " + P2.Age);
+            Console.WriteLine("Weight: " + P2.Weight);
+
         }
     }
 }
